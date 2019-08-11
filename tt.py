d81483891e40897e8ca0f0a18264de19309820e4
xunchanglu0901 <xunchanglu0901@163.com>
2019-08-11 20:32:28 +0800

    modified,remove

--- tt.py
+++ tt.py
@@ -5,17 +5,14 @@
 
 # 库
 import pandas as pd
+from pandas import DataFrame
 import numpy as np
+left1 = DataFrame({'group_val2':[3.5,7]},index=['a2','b2'])
+right1 = DataFrame({'group_val1':[3.5,7]},index=['a1','b1'])
 
-data_pollution = pd.DataFrame(np.arange(12).reshape(4,3), index=list('abcd'), columns=list('xyz'))
-df2 = pd.DataFrame(np.arange(12).reshape(4,3), index=list('abcd'), columns=list('xyz'))
-data_pollution.x = np.nan
-data_pollutionX = data_pollution.copy()
-for columname in data_pollution.columns:
-    if data_pollution[columname].count() != len(data_pollution):
-        loc = data_pollution[columname][data_pollution[columname].isnull().values == True].index.tolist()
-        for nub in loc:
-            data_pollutionX[columname][nub] = df2[columname][nub]
 
-print(data_pollution)
-print(data_pollutionX)
+
+
+d = pd.concat([left1,right1],axis=1, sort=False)
+
+print(d)
