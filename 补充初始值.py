60ece72e43caf74eba7db97d3149a75a3a195f02
xunchanglu0901 <xunchanglu0901@163.com>
2019-08-29 15:17:35 +0800

    modified,added

---
+++ 补充初始值.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# 作者: xcl
+# 时间: 2019/8/29 14:55
+
+
+# 库
+import pandas as pd
+import numpy as np
+
+
+a = [[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan]]
+a = np.array(a)
+a = pd.DataFrame(a)
+a[a.shape[1]%2][a.shape[0]%2] = float(0.00)
+
+print(a)
+
